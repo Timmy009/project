2b4d5741938ad1d7ea76c7990b855d8034f0ce56
Timmy009 <timileyinbamgbose7@gmail.com>
2024-05-06 02:02:04 +0100

    jd

--- src/pages/Slick.js
+++ src/pages/Slick.js
@@ -2,12 +2,18 @@ import React, { useState } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import slickOne from '../assets/slickOne.png'
+import slickTwo from '../assets/slickTwo.png'
+import slickThree from '../assets/slickThree.png'
 
 import './Slick.css';
 import { dataDigitalBestSeller } from './Data';
 import imgGirl from '../assets/defaultImage.jpg';
 
 function Slick() {
+
+
+  const data = [slickOne, slickTwo, slickThree]
   const [defaultImage, setDefaultImage] = useState({});
   const settings = {
     dots: true,
@@ -65,24 +71,20 @@ function Slick() {
   return (
     <div className="App">
       <Slider {...settings}>
-        {dataDigitalBestSeller.map((item) => (
-          <div className="card">
+        {data.map((item) => (
+         
             <div className="card-top">
               <img
                 src={
-                  defaultImage[item.title] === item.title
-                    ? defaultImage.linkDefault
-                    : item.linkImg
+                 
+                     item
                 }
-                alt={item.title}
+              
                 onError={handleErrorImage}
               />
-              <h1>{item.title}</h1>
-            </div>
-            <div className="card-bottom">
-              <h3>{item.price}</h3>
-              <span className="category">{item.category}</span>
-            </div>
+             
+          
+         
           </div>
         ))}
       </Slider>
